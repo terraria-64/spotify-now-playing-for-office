140131f2ca4cde86d68fe9d0aefdb683d70b0e3d
terraria-64 <trk@swimmy.co>
2026-03-31 11:33:12 +0900

    favicon更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,6 @@ const geistSans = Geist({
 export const metadata: Metadata = {
   title: "Spotify Now Playing",
   description: "再生中の曲を表示したりキューに曲を追加できるやつ",
-  icons: {
-    icon: "/sw-favi.png",
-  },
 };
 
 export default function RootLayout({
